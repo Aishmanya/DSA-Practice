0df6db27c4634d3fcd4a0cdb822e8adc0b823e1d
Aishmanya <aishmanyanishi@gmail.com>
2026-07-15 11:55:03 +0530

    Initial commit

---
+++ Arrays/TwoSum.cpp
@@ -0,0 +1,22 @@
+// Problem: Two Sum
+// Link: https://leetcode.com/problems/two-sum/
+// Time: O(n)
+// Space: O(n)
+
+// Approach:
+// Store each element in a hashmap.
+// For every element, check if target - nums[i] exists.
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            int need = target - nums[i];
+            if(mp.count(need))
+                return {mp[need], i};
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};

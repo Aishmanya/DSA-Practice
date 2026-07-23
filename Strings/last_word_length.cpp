882a88cb9ee4eec551df6f8372fb0e9aea7da959
Aishmanya <aishmanyanishi@gmail.com>
2026-07-23 12:43:16 +0530

    Notes added

--- Strings/last_word_length.cpp
+++ Strings/last_word_length.cpp
@@ -1,4 +1,12 @@
+// Problem: Length of Last Word
+// Link: https://leetcode.com/problems/length-of-last-word/
+// Time: O(n)
+// Space: O(1)
 
+// Approach:
+// Iterate backwards from the end of the string.
+// Skip any trailing spaces first.
+// Count characters of the last word until another space is encountered or the beginning of string is reached.
 
 
 

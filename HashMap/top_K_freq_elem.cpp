36d5dd940a281b83b79b65a4836115d67df29f91
Aishmanya <aishmanyanishi@gmail.com>
2026-07-26 23:47:11 +0530

    Top K frequent elements

--- HashMap/top_K_freq_elem.cpp
+++ HashMap/top_K_freq_elem.cpp
@@ -7,3 +7,32 @@
 // 1. Build a frequency map for all elements.
 // 2. Create buckets where index represents the frequency and stores numbers with that frequency.
 // 3. Iterate backward through buckets (from highest frequency down) to collect top K elements.
+
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> countMap;
+        for (int num : nums) {
+            countMap[num]++;
+        }
+        
+        // Bucket index = frequency, Bucket value = list of numbers with that frequency
+        int n = nums.size();
+        vector<vector<int>> buckets(n + 1);
+        for (auto& pair : countMap) {
+            buckets[pair.second].push_back(pair.first);
+        }
+        
+        vector<int> result;
+        // Traverse from back (highest possible frequency down to 1)
+        for (int i = n; i >= 1 && result.size() < k; i--) {
+            for (int num : buckets[i]) {
+                result.push_back(num);
+                if (result.size() == k) break;
+            }
+        }
+        
+        return result;
+    }
+};

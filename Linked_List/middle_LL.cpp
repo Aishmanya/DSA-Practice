2c17fcbe06f46fc3b795edb6038332c592b864fd
Aishmanya <aishmanyanishi@gmail.com>
2026-07-27 23:04:46 +0530

    Reverse and middle of Linked List

--- Linked_List/middle_LL.cpp
+++ Linked_List/middle_LL.cpp
@@ -7,3 +7,26 @@
 // Use Fast and Slow pointers (Tortoise and Hare technique).
 // Slow moves 1 step while Fast moves 2 steps at a time.
 // When Fast reaches the end (or NULL), Slow will be pointing directly at the middle node.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};

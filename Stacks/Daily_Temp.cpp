1b78144a029d802e9d8a9128007f3c914b347ef2
Aishmanya <aishmanyanishi@gmail.com>
2026-08-04 23:00:24 +0530

    Daily Temp

--- Stacks/Daily_Temp.cpp
+++ Stacks/Daily_Temp.cpp
@@ -8,3 +8,25 @@
 // When encountering a warmer temperature, pop indices from the stack 
 // and calculate the difference in days.
 
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> answer(n, 0);
+        stack<int> st; // Stores indices of temperatures
+
+        for (int i = 0; i < n; ++i) {
+            // Process all previous colder days
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int prevIndex = st.top();
+                st.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+            
+            // Push current day's index
+            st.push(i);
+        }
+
+        return answer;
+    }
+};

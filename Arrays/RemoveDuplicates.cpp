12433215fac1da6ea4dc08a6914cd4c71810e5d4
Aishmanya <aishmanyanishi@gmail.com>
2026-08-20 01:41:36 +0530

    Code corrected

--- Arrays/RemoveDuplicates.cpp
+++ Arrays/RemoveDuplicates.cpp
@@ -8,16 +8,20 @@
 // The slow pointer 'i' tracks the position of the last unique element found.
 // The fast pointer 'j' scans the array to find new unique elements and move them forward to i.
 
-class Solution {
+class Solution
+{
 public:
-int removeDuplicates(vector& nums) {
-int i=0;
-for(int j=1;j<nums.size();j++){
-if(nums[j] != nums[i]){
-nums[i+1] = nums[j] ;
-i++;
-}
-}
-return i+1;
-}
+    int removeDuplicates(vector &nums)
+    {
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++)
+        {
+            if (nums[j] != nums[i])
+            {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+        }
+        return i + 1;
+    }
 };
